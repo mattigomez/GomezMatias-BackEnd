de4ac16250e84ccdabac7b6839cdad9b88ef906f
MatiasGomez <gomezmati@hotmail.com>
2023-07-30 14:09:41 -0300

    arreglamos algunos errores de cookies

--- src/config/admin.config.js
+++ src/config/admin.config.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 module.exports = {
     admin_email: process.env.ADMIN_EMAIL,
     admin_password: process.env.ADMIN_PASSWORD,
-    testerEmail: process.env.TESTER_EMAIL,
-    testerPassword: process.env.TESTE_PASSWORD
+    tester_email: process.env.TESTER_EMAIL,
+    tester_password: process.env.TESTER_PASSWORD
 }
