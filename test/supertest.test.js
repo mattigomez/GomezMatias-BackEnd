1e00887e70f73e279133aecb6a1a141bfa76fc1c
MatiasGomez <gomezmati@hotmail.com>
2023-08-23 19:41:10 -0300

    arreglos en test

--- test/supertest.test.js
+++ test/supertest.test.js
@@ -10,8 +10,6 @@ const requester = supertest(app)
 let cookies // Variable global para almacenar las cookies de sesión
  
 describe('test de productos', () => {
-
-
   before(async function() {
     this.timeout(10000) //tiempo de espera para el hook before
 
@@ -21,11 +19,6 @@ describe('test de productos', () => {
         .post('/api/login')
         .send({ email: tester_email, password: tester_password })
       cookies = resLogin.headers['set-cookie']
-      
-      console.log(tester_email)
-      console.log(tester_password)
-      console.log('Cookies:', cookies); // Agregar este mensaje para verificar las cookies
-
 
        // Agrega una verificación para asegurarte de que las cookies se hayan establecido correctamente
        if (!cookies) {
@@ -37,13 +30,7 @@ describe('test de productos', () => {
     }
   })
 
-  it('El endpoint GET /api/dbProducts debe de mostrar todos los productos ', async () => {
-    
-    // Verifica si las cookies están definidas antes de hacer la solicitud
-    if (!cookies) {
-      throw new Error('Las cookies no están definidas. La autenticación puede haber fallado en la prueba "before".')
-    }
-
+  it('El endpoint GET /api/dbProducts debe de mostrar todos los productos con o sin filtros', async () => {
     // Agrega las cookies de sesión a la solicitud de productos
     const getProducts = await requester
       .get('/api/dbProducts')
@@ -51,4 +38,100 @@ describe('test de productos', () => {
 
     expect(getProducts.status).to.equal(200)
   });
-})
+
+  it('El endpoint POST /api/dbProducts agrega un nuevo producto a la base de datos', async () => {
+
+    const randomProduct = {
+      name: 'producto de prueba',
+      description: 'descripcion del producto de prueba',
+      price: 1124,
+      stock: 100,
+      category: 'dispositivos',
+      code: '12ewh1-1232-d123df',
+      status: true,
+      img: 'https://imagenRandom.com',
+      owner: 'administrador'
+    }
+
+
+    const addProduct = await requester
+      .post('/api/dbProducts')
+      .send(randomProduct)
+      .set('Cookie', cookies)
+    
+    expect(addProduct.status).to.equal(200)
+  })
+
+
+  it('El endpoint PUT /api/dbProducts/:productId actualiza un producto en la base de datos', async () => {
+    // Agrega un producto inicialmente a la base de datos para obtener su ID
+    const randomProduct = {
+      name: 'producto a actualizar',
+      description: 'descripcion del producto a actualizar',
+      price: 500,
+      stock: 50,
+      category: 'actualizables',
+      code: 'abcd-1234',
+      status: true,
+      img: 'https://imagenRandom.com',
+      owner: 'administrador'
+    }
+
+    const addProduct = await requester
+      .post('/api/dbProducts')
+      .send(randomProduct)
+      .set('Cookie', cookies)
+
+    const productId = addProduct.body.message._id
+
+
+    // Datos actualizados del producto
+    const updatedProductData = {
+      name: 'producto actualizado',
+      description: 'descripcion del producto actualizado',
+      price: 1000,
+      stock: 20,
+      category: 'actualizables',
+      code: 'abcd-1234',
+      status: false,
+      img: 'https://imagenActualizada.com/productoactualizado.jpeg',
+      owner: 'administrador'
+    }
+
+    const updateProduct = await requester
+      .put(`/api/dbProducts/${productId}`)
+      .send(updatedProductData)
+      .set('Cookie', cookies)
+
+    expect(updateProduct.status).to.equal(200);
+  });
+
+  it('El endpoint DELETE /api/dbProducts/:productId elimina un producto de la base de datos', async () => {
+    // Agrega un producto inicialmente a la base de datos para obtener su id
+    const randomProduct = {
+      name: 'producto a eliminar',
+      description: 'descripcion del producto a eliminar',
+      price: 100,
+      stock: 10,
+      category: 'eliminables',
+      code: 'abcd-5678',
+      status: true,
+      img: 'https://imagenRandom.com',
+      owner: 'administrador'
+    }
+
+    const addProduct = await requester
+      .post('/api/dbProducts')
+      .send(randomProduct)
+      .set('Cookie', cookies)
+
+    const productId = addProduct.body.message._id;
+
+    const deleteProduct = await requester
+      .delete(`/api/dbProducts/${productId}`)
+      .set('Cookie', cookies)
+
+    expect(deleteProduct.status).to.equal(200);
+  });
+
+});
